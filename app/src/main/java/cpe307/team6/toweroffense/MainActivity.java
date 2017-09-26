767138e6e081529cadcb839ac16ad9b136922aec
Kent Kawahara <kkawahara1028@gmail.com>
2017-09-25 22:21:48 -0700

    add android boiler plate

---
+++ app/src/main/java/cpe307/team6/toweroffense/MainActivity.java
@@ -0,0 +1,13 @@
+package cpe307.team6.toweroffense;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
