4bcf4775b28b3eeffc763830e932a957ae555ec2
aross885 <32276741+aross885@users.noreply.github.com>
2017-11-13 11:37:29 -0800

    creating the map class Issue #45

---
+++ app/src/main/java/cpe307/team6/toweroffense/Map-Class.java
@@ -0,0 +1,21 @@
+public class Map {
+   private int width;
+   private int height;
+   private List<Location> path;
+   private List<List<Tile>> map;
+
+   public Tile getTile(int x, int y){}
+
+   private int getWidth(){}
+   private void setWidth(){}
+
+   private int getHeight(){}
+   private void setHeight(){}
+
+   private List<Location> getPath(){}
+   private void setPath(){}
+
+
+   
+
+}
