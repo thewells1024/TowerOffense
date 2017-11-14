b65194cf14bf758df50f7c7db7bc35d5dc386907
primitiveDataType <amjroger@gmail.com>
2017-11-14 13:52:35 -0800

    [Issue #39] Removed comments that don't explain 'why' according to PR feedback.

--- Tower.java
+++ Tower.java
@@ -7,13 +7,7 @@ public interface Tower{
 		DISTANCE, HEALTH, FIRST, LAST
 	};
 	
-	/*
-	*	Sets priority to given priority.
-	*/
 	public void setPriority(Priority priority);
 
-
-	/* Attack 0 or more units in the list of units
-	*/
 	public void attack(List<Unit> units);
 }
