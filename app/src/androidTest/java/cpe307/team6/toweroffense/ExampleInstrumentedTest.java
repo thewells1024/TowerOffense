b47f0586d5cdc79d837fdc333d41d8d3d93a3f80
Kent Kawahara <kkawahara1028@gmail.com>
2017-11-14 13:26:27 -0800

    Add ability for build to run checkstyle
    ticket "https://github.com/thewells1024/TowerOffense/issues/53"

--- app/src/androidTest/java/cpe307/team6/toweroffense/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/cpe307/team6/toweroffense/ExampleInstrumentedTest.java
@@ -1,13 +1,13 @@
 package cpe307.team6.toweroffense;
 
+import static org.junit.Assert.assertEquals;
+
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +16,11 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+   @Test
+   public void useAppContext() throws Exception {
+      // Context of the app under test.
+      final Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("cpe307.team6.toweroffense", appContext.getPackageName());
-    }
+      assertEquals("cpe307.team6.toweroffense", appContext.getPackageName());
+   }
 }
