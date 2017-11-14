6c779937c8f71e4a27a8572b30477c8c316cdcbd
aross885 <32276741+aross885@users.noreply.github.com>
2017-11-14 11:44:00 -0800

    Made changes to the Map Class Issues #45

--- app/src/main/java/cpe307/team6/toweroffense/Map.java
+++ app/src/main/java/cpe307/team6/toweroffense/Map.java
@@ -1,39 +1,28 @@
+import java.util.List;
 public class Map {
-   private int width;
-   private int height;
-   private List<Location> path;
+   private final int width;
+   private final int height;
+   private final List<Location> path;
    private List<List<Tile>> map;
 
-   public Tile getTile(int x, int y){}
-
-   public void setWidth(int width) {
+   public Map( int width, int height, List<Location> path,List<List<Tile>> map) {
       this.width = width;
-   }
-
-   public void setHeight(int height) {
       this.height = height;
-   }
-
-   public void setPath(List<Location> path) {
       this.path = path;
-   }
-
-   public void setMap(List<List<Tile>> map) {
       this.map = map;
    }
-
+   public Tile getTile(int x, int y){
+      return map[x][y];
+   }
    public int getWidth() {
       return width;
    }
-
    public int getHeight() {
       return height;
    }
-
    public List<Location> getPath() {
       return path;
    }
-
    public List<List<Tile>> getMap() {
       return map;
    }
