a496f6c41b4a3fbeadd48ddc86448c4a246d18a7
Kent Kawahara <kkawahara1028@gmail.com>
2017-11-14 17:56:17 -0800

    Fix issue with Unit interface
    ticket "https://github.com/thewells1024/TowerOffense/issues/64"

--- app/src/main/java/cpe307/team6/toweroffense/game/interfaces/Unit.java
+++ app/src/main/java/cpe307/team6/toweroffense/game/interfaces/Unit.java
@@ -3,8 +3,8 @@ package cpe307.team6.toweroffense.game.interfaces;
 import java.util.List;
 
 public interface Unit {
-   public Location move(List<Location>);
+   public Location move(List<Location> path);
    public Location getLocation();
    public int getAttack();
-   public boolean takeDamage(int);
+   public boolean takeDamage(int amount);
 }
