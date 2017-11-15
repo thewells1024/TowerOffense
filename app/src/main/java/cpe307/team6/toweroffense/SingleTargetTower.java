b4cb91d41a49f58f94c15400bc998bdd3bd5533d
Chris Varanese <cvaranes@calpoly.edu>
2017-11-15 12:28:33 -0800

    added the suggested changes

---
+++ app/src/main/java/cpe307/team6/toweroffense/SingleTargetTower.java
@@ -0,0 +1,22 @@
+/* design taken from the not-yet-pulled tower interface */
+
+import java.util.ArrayList;
+
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
+public class SingleTargetTower implements Tower {
+
+   @Getter private final Location location;
+   private final static int attackDistance;
+   private final static int damage;
+
+   /* returns the first unit in the list that is within the tower's range */
+   public Unit selectTargetByPriority(ArrayList<Unit> units){
+	   /* assuming this list is organized by priority already */
+	   for(Unit unit: units) {
+		   if(unit.getLocation().getDistance(this.location) < attackDistance) {
+			   	return unit;
+		   }
+	   }
+   }
+
+}
