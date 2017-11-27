7c70982cfe6176160c5505c23bb7269e92460a0c
Kent Kawahara <kkawahara1028@gmail.com>
2017-11-27 11:46:17 -0800

    Checkstyle (#70)
    * Add Checkstyle configuration
    
    ticket "https://github.com/thewells1024/TowerOffense/issues/53"
    
    * Add checkstyle configuration
    
    ticket "https://github.com/thewells1024/TowerOffense/issues/53"
    
    * Add ability for build to run checkstyle
    
    ticket "https://github.com/thewells1024/TowerOffense/issues/53"

--- app/src/main/java/cpe307/team6/toweroffense/StartScreen.java
+++ app/src/main/java/cpe307/team6/toweroffense/StartScreen.java
@@ -1,13 +1,13 @@
 package cpe307.team6.toweroffense;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class StartScreen extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_screen);
-    }
+   @Override
+   protected void onCreate(final Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_start_screen);
+   }
 }
