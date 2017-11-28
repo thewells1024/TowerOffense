a07bf517de33860bf3c9c821a7dc8d97a23e6626
Kent Kawahara <kkawahara1028@gmail.com>
2017-11-27 21:08:58 -0800

    Add Base Class
    ticket "https://github.com/thewells1024/TowerOffense/issues/79"

---
+++ app/src/test/java/cpe307/team6/toweroffense/game/BaseTest.java
@@ -0,0 +1,15 @@
+package cpe307.team6.toweroffense.game;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class BaseTest {
+   @Test
+   public void testTakeDamage() {
+      Base base = new Base(300, new Location(0,0), Collections.<Location>emptyList());
+      assertEquals(200, base.takeDamage(100));
+   }
+}
