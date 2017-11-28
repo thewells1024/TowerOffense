48f7cd2efa6387aa3c6c835adac8f0c7d501dc70
Kent Kawahara <kkawahara1028@gmail.com>
2017-11-27 21:18:53 -0800

    Add Base Class (#80)
    ticket "https://github.com/thewells1024/TowerOffense/issues/79"

---
+++ app/src/main/java/cpe307/team6/toweroffense/game/Base.java
@@ -0,0 +1,27 @@
+package cpe307.team6.toweroffense.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+public class Base {
+   @Setter(AccessLevel.NONE)
+   private int health;
+   private final Location location;
+   private final List<Location> path;
+
+   public int takeDamage(final int amount) {
+      health -= amount;
+      return health;
+   }
+
+   public List<Location> getPath() {
+      return new ArrayList<>(path);
+   }
+}
