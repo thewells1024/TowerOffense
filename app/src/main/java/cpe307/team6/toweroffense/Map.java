88764101e9613bf2131def4e02203c3d7f8d6fdb
aross885 <32276741+aross885@users.noreply.github.com>
2017-11-29 16:20:21 -0800

    removed clone variables, addressed map constructor, passed in canHoldTower, finalized arguments #45

--- app/src/main/java/cpe307/team6/toweroffense/Map.java
+++ app/src/main/java/cpe307/team6/toweroffense/Map.java
@@ -7,23 +7,24 @@ import lombok.Getter;
 public class Map {
    @Getter private final int width;
    @Getter private  final int height;
-   @Getter private final List<Location> path;
+   private final List<Location> path;
    private List<List<Tile>> map;
 
    public Map(final int width, final int height, final List<Location> path) {
-      this.width = width;
-      this.height = height;
-      this.path = path;
+      map.width = width;
+      map.height = height;
+      map.path = path;
    }
 
    private class Tile extends Location {
       private boolean hasTower;
-      private boolean canHoldTower;
+      private final boolean canHoldTower;
 
-      public Tile (final int x, final int y) {
+      public Tile (final int x, final int y, final boolean canHoldTower) {
          this.x = x;
          this.y = y;
          this.hasTower = false;
+         this.canHoldTower = canHoldTower;
       }
 
       public boolean hasTower() {
@@ -35,13 +36,11 @@ public class Map {
       }
    }
 
-   public Tile getTile(int x, int y) {
-      Tile clone = (Tile) map.get(x).get(y).clone();
-      return clone;
+   public Tile getTile(final int x, final int y) {
+      return (Tile) map.get(x).get(y).clone();
    }
 
    public List<Location> getPath() {
-      List<Location> newPath = path.clone();
-      return newPath;
+      return  path.clone();
    }
 }
