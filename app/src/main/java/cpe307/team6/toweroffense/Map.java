4f53903d82688f6ab40bcabae17b1fc79a7021e5
aross885 <32276741+aross885@users.noreply.github.com>
2017-12-02 14:58:03 -0800

    Aross08 Issue #45 (#68)
    https://github.com/thewells1024/TowerOffense/issues/45

---
+++ app/src/main/java/cpe307/team6/toweroffense/Map.java
@@ -0,0 +1,62 @@
+package cpe307.team6.toweroffense.game;
+
+import java.util.ArrayList;
+import java.util.List;
+import cpe307.team6.toweroffense.game.Location;
+import lombok.Getter;
+
+public class Map {
+   @Getter private final int width;
+   @Getter private final int height;
+   private final List<Location> path;
+   private List<List<Tile>> map;
+
+   public Map(final int width, final int height, final List<Location> path) {
+      this.width = width;
+      this.height = height;
+      this.path = path.clone();
+      map = createMap(width, height, path);
+   }
+   
+   public static List<List<Tile>> createMap(final int width, final int height, final List<Location> path) {
+      List<List<Tile>> returnMap = new ArrayList<>();
+      for ( int i = 0; i < height; i++) {
+         ArrayList<Tile> row = new ArrayList<>();
+         for (int j = 0; j < width; j++) {
+            row.add(new Tile(i,j,!path.contains(new Location(i,j))));           
+         }
+         returnMap.add(row);
+      }
+      return returnMap;
+   }
+   
+   @Data
+   public class Tile extends Location implements Cloneable {
+      private boolean hasTower;
+      private final boolean canHoldTower;
+      
+      public Tile (final int x, final int y, final boolean canHoldTower) {
+         super(x,y);
+         this.hasTower = false;
+         this.canHoldTower = canHoldTower;
+      }
+
+      public Tile (final int x, final int y, boolean hasTower, final boolean canHoldTower) {
+         super(x,y);
+         this.hasTower = hasTower;
+         this. canHoldTower = canHoldTower;
+      }
+
+      public Object clone() throws CloneNotSupportedException {
+         return new Tile(x, y, hasTower, canHoldTower);
+      }
+   }
+
+   public Tile getTile(final int x, final int y) {
+      return (Tile) map.get(x).get(y).clone();
+   }
+
+   public List<Location> getPath() {
+      return  path.clone();
+   }
+}
