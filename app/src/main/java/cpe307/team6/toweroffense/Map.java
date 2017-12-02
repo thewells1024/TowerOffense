a55051ded9bf827cbb05d65fc8c54b0260bcc3cc
aross885 <32276741+aross885@users.noreply.github.com>
2017-12-02 13:06:44 -0800

    finally fixed constructor, made changes to Tile cLass #45

--- app/src/main/java/cpe307/team6/toweroffense/Map.java
+++ app/src/main/java/cpe307/team6/toweroffense/Map.java
@@ -1,38 +1,48 @@
 package cpe307.team6.toweroffense.game;
 
+import java.util.ArrayList;
 import java.util.List;
 import cpe307.team6.toweroffense.game.Location;
 import lombok.Getter;
 
 public class Map {
    @Getter private final int width;
-   @Getter private  final int height;
+   @Getter private final int height;
    private final List<Location> path;
    private List<List<Tile>> map;
 
    public Map(final int width, final int height, final List<Location> path) {
-      map.width = width;
-      map.height = height;
-      map.path = path;
+      this.width = width;
+      this.height = height;
+      this.path = path.clone();
+      map = createMap(width, height, path);
    }
-
-   private class Tile extends Location {
+   
+   public List<List<Tile>> createMap(final int width, final int height, final List<Location> path) {
+      List<List<Tile>> returnMap = new ArrayList<>();
+      for ( int i = 0; i < height; i++) {
+         ArrayList<Tile> row = new ArrayList<>();
+         for (int j = 0; j < width; j++) {
+            row.add(new Tile(i,j,path.contains(new Location(i,j))));           
+         }
+         returnMap.add(row);
+      }
+      return returnMap;
+   }
+   
+   @Data
+   private class Tile extends Location implements Cloneable {
       private boolean hasTower;
       private final boolean canHoldTower;
-
+      
       public Tile (final int x, final int y, final boolean canHoldTower) {
-         this.x = x;
-         this.y = y;
+         super(x,y);
          this.hasTower = false;
          this.canHoldTower = canHoldTower;
       }
 
-      public boolean hasTower() {
-         return this.hasTower;
-      }
-
-      public boolean canHoldTower() {
-         return this.canHoldTower;
+      public Object clone() throws CloneNotSupportedException {
+         return super.clone();
       }
    }
 
