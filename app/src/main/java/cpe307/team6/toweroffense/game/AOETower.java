3c109ef0d6e4d86fd7b14db16240af13b21617eb
primitiveDataType <amjroger@gmail.com>
2017-12-03 10:43:59 -0800

    Issue #60, made changes to correct checkstyle failures

--- app/src/main/java/cpe307/team6/toweroffense/game/AOETower.java
+++ app/src/main/java/cpe307/team6/toweroffense/game/AOETower.java
@@ -1,108 +1,108 @@
 package cpe307.team6.toweroffense.game;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Arrays;
-import java.util.Collections;
-import cpe307.team6.toweroffense.game.Location;
+
 import cpe307.team6.toweroffense.game.interfaces.Unit;
 import cpe307.team6.toweroffense.game.interfaces.Tower;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AOETower implements Tower {
 
-    public Tower.Priority priority;
-    public Location loc;
+   public Tower.Priority priority;
+   public Location loc;
 
-    public AOETower(Location loc) {
-        this.priority = Tower.Priority.DISTANCE;
-        this.loc = loc;
-    }
+   public AOETower(final Location loc) {
+      this.priority = Tower.Priority.DISTANCE;
+      this.loc = loc;
+   }
 
-    public AOETower(Location loc, Tower.Priority priority) {
-        this.priority = priority;
-        this.loc = loc;
-    }
+   public AOETower(final Location loc, final Tower.Priority priority) {
+      this.priority = priority;
+      this.loc = loc;
+   }
 
-    private Unit getWeakestUnit(List<Unit> units, int numUnits) {
-        //Assumes numUnits > 1
-        Unit weakestUnit = units.get(0);
-        double currentUnitHealth;
-        double weakestUnitHealth = weakestUnit.getHealth();
+   private Unit getWeakestUnit(final List<Unit> units, final int numUnits) {
+      //Assumes numUnits > 1
+      Unit weakestUnit = units.get(0);
+      double currentUnitHealth;
+      double weakestUnitHealth = weakestUnit.getHealth();
 
-        for (int i = 1; i < numUnits; i++) {
-            currentUnitHealth = units.get(i).getHealth();
-            if (currentUnitHealth < weakestUnitHealth) {
-                weakestUnitHealth = currentUnitHealth;
-                weakestUnit = units.get(i);
-            }
-        }
+      for (int i = 1; i < numUnits; i++) {
+         currentUnitHealth = units.get(i).getHealth();
+         if (currentUnitHealth < weakestUnitHealth) {
+            weakestUnitHealth = currentUnitHealth;
+            weakestUnit = units.get(i);
+         }
+      }
 
-        return weakestUnit;
-    }
+      return weakestUnit;
+   }
 
-    private Unit getNearestUnit(List<Unit> units, int numUnits) {
-        //Assumes numUnits > 1
-        Unit closestUnit = units.get(0);
-        double currentUnitDistance;
-        double shortestDistance = this.getLocation().getDistance(units.get(0).getLocation());
+   private Unit getNearestUnit(final List<Unit> units, final int numUnits) {
+      //Assumes numUnits > 1
+      Unit closestUnit = units.get(0);
+      double currentUnitDistance;
+      double shortestDistance =
+            this.getLocation().getDistance(units.get(0).getLocation());
 
-        for (int i = 1; i < numUnits; i++) {
-            currentUnitDistance = this.getLocation().getDistance(units.get(i).getLocation());
+      for (int i = 1; i < numUnits; i++) {
+         currentUnitDistance =
+               this.getLocation().getDistance(units.get(i).getLocation());
 
-            // Reset ptr to closer unit
-            if (currentUnitDistance < shortestDistance) {
-                closestUnit = units.get(i);
-                shortestDistance = currentUnitDistance;
-            }
-        }
+         // Reset ptr to closer unit
+         if (currentUnitDistance < shortestDistance) {
+            closestUnit = units.get(i);
+            shortestDistance = currentUnitDistance;
+         }
+      }
 
-        return closestUnit;
-    }
+      return closestUnit;
+   }
 
-    public void setPriority(Tower.Priority priority) {
-        this.priority = priority;
-    }
+   public void setPriority(final Tower.Priority priority) {
+      this.priority = priority;
+   }
 
-    public Tower.Priority getPriority() {
-        return this.priority;
-    }
+   public Tower.Priority getPriority() {
+      return this.priority;
+   }
 
-    /* Handles Priorities: DISTANCE and HEALTH
-     * DISTANCE:
-     *  -> Attacks Unit that is the shortest distance from this tower
-     * HEALTH:
-     * -> Attacks Health that is the shortest distance from this tower
-     */
-    public List<Unit> selectTargets(List<Unit> units) {
-        Unit target = null;
-        int numUnits = units.size();
-        if (numUnits == 0) {
-            ; //throw new Exception("No units to set as target. List of units is empty");
-        }
-        if (numUnits == 1) {
-            target = units.get(0);
-        }
-        else if (this.priority == Tower.Priority.DISTANCE) {
-            target = getNearestUnit(units, numUnits);
-        }
-        else if (this.priority == Tower.Priority.HEALTH) {
-            //Find unit that has the lowest health
-            target = getWeakestUnit(units, numUnits);
-        }
-        else if (this.priority == Tower.Priority.FIRST) {
-            target = units.get(0);
-        }
-        else if (this.priority == Tower.Priority.LAST) {
-            target = units.get(numUnits - 1);
-        }
+   /* Handles Priorities: DISTANCE and HEALTH
+   * * DISTANCE:
+   *  -> Attacks Unit that is the shortest distance from this tower
+   * HEALTH:
+   * -> Attacks Health that is the shortest distance from this tower
+   */
+   public List<Unit> selectTargets(final List<Unit> units) {
+      Unit target = null;
+      final int numUnits = units.size();
 
-        return target != null ? Collections.singletonList(target) :
-                new ArrayList<Unit>();
-    }
+      if (numUnits == 0) {
+         //"No units to set as target. List of units is empty
+      }
+      else if (numUnits == 1) {
+         target = units.get(0);
+      }
+      else if (this.priority == Tower.Priority.DISTANCE) {
+         target = getNearestUnit(units, numUnits);
+      }
+      else if (this.priority == Tower.Priority.HEALTH) {
+         //Find unit that has the lowest health
+         target = getWeakestUnit(units, numUnits);
+      }
+      else if (this.priority == Tower.Priority.FIRST) {
+         target = units.get(0);
+      }
+      else if (this.priority == Tower.Priority.LAST) {
+         target = units.get(numUnits - 1);
+      }
 
-    public Location getLocation() {
-        return this.loc;
-    }
+      return target != null ? Collections.singletonList(target) :
+            new ArrayList<Unit>();
+   }
+
+   public Location getLocation() {
+      return this.loc;
+   }
 }
--- app/src/test/java/cpe307/team6/toweroffense/game/AOETowerTest.java
+++ app/src/test/java/cpe307/team6/toweroffense/game/AOETowerTest.java
@@ -20,84 +20,85 @@ import static org.mockito.Mockito.when;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class AOETowerTest {
-    private Tower.Priority priority;
-    private int x, y;
-    private Location loc;
-    Unit unit1, unit2, unit3;
-    @Before
-    public void setup() {
-        priority = Tower.Priority.HEALTH;
-        x = 3;
-        y = 4;
-        loc = new Location(x, y);
-        unit1 = mock(Unit.class);
-        unit2 = mock(Unit.class);
-        unit3 = mock(Unit.class);
-    }
+   private Tower.Priority priority;
+   private int x, y;
+   private Location loc;
+   Unit unit1, unit2, unit3;
 
-    @Test
-    public void testAOETowerConstructor() {
-        AOETower testTower = new AOETower(loc, priority);
-        assertEquals(testTower.loc.getX(), x, .005);
-        assertEquals(testTower.loc.getY(), y, .005);
-        assertEquals(testTower.getPriority(), priority);
-    }
+   @Before
+   public void setup() {
+      priority = Tower.Priority.HEALTH;
+      x = 3;
+      y = 4;
+      loc = new Location(x, y);
+      unit1 = mock(Unit.class);
+      unit2 = mock(Unit.class);
+      unit3 = mock(Unit.class);
+   }
 
-    @Test
-    public void testAOETowerDefaultPriority() {
-        AOETower testTower = new AOETower(loc);
-        assertEquals(testTower.getPriority(), Tower.Priority.DISTANCE);
-    }
+   @Test
+   public void testAOETowerConstructor() {
+      AOETower testTower = new AOETower(loc, priority);
+      assertEquals(testTower.loc.getX(), x, .005);
+      assertEquals(testTower.loc.getY(), y, .005);
+      assertEquals(testTower.getPriority(), priority);
+   }
 
-    @Test
-    public void testAOETowerSetPriority() {
-        Tower.Priority newPriority = Tower.Priority.FIRST;
-        AOETower testTower = new AOETower(loc);
-        assertNotEquals(testTower.getPriority(), newPriority);
-        testTower.setPriority(newPriority);
-        assertEquals(testTower.getPriority(), newPriority);
-    }
+   @Test
+   public void testAOETowerDefaultPriority() {
+      AOETower testTower = new AOETower(loc);
+      assertEquals(testTower.getPriority(), Tower.Priority.DISTANCE);
+   }
 
-    @Test
-    public void testAOETowerSelectTargetsByHealthOfEqual() {
-        when(unit1.getHealth()).thenReturn(10);
-        when(unit2.getHealth()).thenReturn(6);
-        when(unit3.getHealth()).thenReturn(6);
+   @Test
+   public void testAOETowerSetPriority() {
+      Tower.Priority newPriority = Tower.Priority.FIRST;
+      AOETower testTower = new AOETower(loc);
+      assertNotEquals(testTower.getPriority(), newPriority);
+      testTower.setPriority(newPriority);
+      assertEquals(testTower.getPriority(), newPriority);
+   }
 
-        List<Unit> units = Arrays.asList(unit1, unit2, unit3);
-        AOETower testTower = new AOETower(loc);
-        testTower.setPriority(Tower.Priority.HEALTH);
-        assertEquals(testTower.selectTargets(units).get(0).getHealth(),
-                unit2.getHealth());
-    }
+   @Test
+   public void testAOETowerSelectTargetsByHealthOfEqual() {
+      when(unit1.getHealth()).thenReturn(10);
+      when(unit2.getHealth()).thenReturn(6);
+      when(unit3.getHealth()).thenReturn(6);
 
-    @Test
-    public void testAOETowerSelectTargetsByHealthOfThree() {
-        when(unit1.getHealth()).thenReturn(10);
-        when(unit2.getHealth()).thenReturn(6);
-        when(unit3.getHealth()).thenReturn(8);
-        List<Unit> units = Arrays.asList(unit1, unit2, unit3);
+      List<Unit> units = Arrays.asList(unit1, unit2, unit3);
+      AOETower testTower = new AOETower(loc);
+      testTower.setPriority(Tower.Priority.HEALTH);
+      assertEquals(testTower.selectTargets(units).get(0).getHealth(),
+            unit2.getHealth());
+   }
 
-        AOETower testTower = new AOETower(loc);
-        testTower.setPriority(Tower.Priority.HEALTH);
+   @Test
+   public void testAOETowerSelectTargetsByHealthOfThree() {
+      when(unit1.getHealth()).thenReturn(10);
+      when(unit2.getHealth()).thenReturn(6);
+      when(unit3.getHealth()).thenReturn(8);
+      List<Unit> units = Arrays.asList(unit1, unit2, unit3);
 
-        assertEquals(testTower.selectTargets(units).get(0).getHealth(),
-                unit2.getHealth());
-    }
+      AOETower testTower = new AOETower(loc);
+      testTower.setPriority(Tower.Priority.HEALTH);
 
-    @Test
-    public void testAOETowerSelectTargetsByDistance() {
-        AOETower testTower = new AOETower(loc);
-        when(unit1.getLocation()).thenReturn(new Location(0, 0));
-        when(unit1.getHealth()).thenReturn(1);
+      assertEquals(testTower.selectTargets(units).get(0).getHealth(),
+            unit2.getHealth());
+   }
 
-        when(unit2.getLocation()).thenReturn(new Location(2, 3));
-        when(unit2.getHealth()).thenReturn(2);
-        List<Unit> units = Arrays.asList(unit1, unit2);
+   @Test
+   public void testAOETowerSelectTargetsByDistance() {
+      AOETower testTower = new AOETower(loc);
+      when(unit1.getLocation()).thenReturn(new Location(0, 0));
+      when(unit1.getHealth()).thenReturn(1);
 
-        testTower.setPriority(Tower.Priority.DISTANCE);
+      when(unit2.getLocation()).thenReturn(new Location(2, 3));
+      when(unit2.getHealth()).thenReturn(2);
+      List<Unit> units = Arrays.asList(unit1, unit2);
+      testTower.setPriority(Tower.Priority.DISTANCE);
 
-        assertEquals(testTower.selectTargets(units).get(0).getHealth(), unit2.getHealth());
-    }
+      assertEquals(testTower.selectTargets(units).get(0).getHealth(),
+            unit2.getHealth());
+   }
 }
 
