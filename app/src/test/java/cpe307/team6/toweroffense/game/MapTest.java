423e2e891b6c01dbb5483d6229be6438d85285c1
Kent Kawahara <kkawahara1028@gmail.com>
2017-12-02 16:10:26 -0800

    Add tests for Map
    ticket "https://github.com/thewells1024/TowerOffense/issues/91"

---
+++ app/src/test/java/cpe307/team6/toweroffense/game/MapTest.java
@@ -0,0 +1,24 @@
+package cpe307.team6.toweroffense.game;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class MapTest {
+   @Test
+   public void testCreateMap() {
+      List<List<Map.Tile>> createdMap = Map.createMap(2, 2, asList(new Location(0,0),
+         new Location(0,1)));
+      assertEquals(2, createdMap.size());
+      createdMap.forEach(row -> assertEquals(2, row.size()));
+      assertFalse(createdMap.get(0).get(0).canHoldTower());
+      assertFalse(createdMap.get(0).get(1).canHoldTower());
+      assertTrue(createdMap.get(1).get(0).canHoldTower());
+      assertTrue(createdMap.get(1).get(1).canHoldTower());
+   }
+}
