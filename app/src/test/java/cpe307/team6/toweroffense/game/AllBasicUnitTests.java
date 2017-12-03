d34f02bcee81b582c88e0a1007568922c9cce317
itwatts <itwatts@calpoly.edu>
2017-12-02 23:46:48 -0800

    Added placeholder classes for unit path testing.  These may not pass the travis checks in their initial states.

---
+++ app/src/test/java/cpe307/team6/toweroffense/game/AllBasicUnitTests.java
@@ -0,0 +1,12 @@
+package cpe307.team6.toweroffense.game;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BasicUnitHealthTest.class, BasicUnitNegativeXTest.class, BasicUnitNegativeYTest.class,
+   BasicUnitPositiveXTest.class, BasicUnitPositiveYTest.class, BasicUnitShortTurnTest.class, BasicUnitTurnTest.class})
+public class AllBasicUnitTests {
+
+}
