56d7d66a7309c658baf17ee3ae38cf1142151769
itwatts <itwatts@calpoly.edu>
2017-12-02 23:51:06 -0800

    Class name error resolved

--- app/src/test/java/cpe307/team6/toweroffense/game/BasicUnitNegativeYTest.java
+++ app/src/test/java/cpe307/team6/toweroffense/game/BasicUnitNegativeYTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class BasicUnitHealthTest {
+public class BasicUnitNegativeYTest {
    @Test
    public void testGetDistance() {
       BasicUnit testUnit = new BasicUnit(null, null, 1000);
--- app/src/test/java/cpe307/team6/toweroffense/game/BasicUnitTurnTest.java
+++ app/src/test/java/cpe307/team6/toweroffense/game/BasicUnitTurnTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class BasicUnitHealthTest {
+public class BasicUnitTurnTest {
    @Test
    public void testGetDistance() {
       BasicUnit testUnit = new BasicUnit(null, null, 1000);
