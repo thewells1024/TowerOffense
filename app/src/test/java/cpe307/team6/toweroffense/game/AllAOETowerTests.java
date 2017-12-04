84a58339aff48a8418b8c85385aac5f013282def
primitiveDataType <amjroger@gmail.com>
2017-12-03 18:12:07 -0800

    #Issue 60 Made changes according to check-style build errors (#96)
    ticket "https://github.com/thewells1024/TowerOffense/issues/60"

---
+++ app/src/test/java/cpe307/team6/toweroffense/game/AllAOETowerTests.java
@@ -0,0 +1,16 @@
+package cpe307.team6.toweroffense.game;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Test Suite for AOETower
+ * Person Responsible: Annamarie Roger
+ */
+@RunWith(Suite.class)
+@SuiteClasses({AOETowerConstructorTest.class, AOETowerPriorityTest.class,
+                    AOETowerDistanceTest.class, AOETowerHealthTest.class})
+public class AllAOETowerTests {
+
+}
+
