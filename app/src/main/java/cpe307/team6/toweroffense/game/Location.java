8ae853e89d8a624d24c3b4acc531f7b11890f04d
primitiveDataType <amjroger@gmail.com>
2017-12-03 17:27:29 -0800

    Issue #60 fixing compiling errors

--- app/src/main/java/cpe307/team6/toweroffense/game/Location.java
+++ app/src/main/java/cpe307/team6/toweroffense/game/Location.java
@@ -10,20 +10,7 @@ public class Location {
    private final double x;
    private final double y;
 
-   public Location(final double xcoordinate, final double ycoordinate) {
-      this.x = xcoordinate;
-      this.y = ycoordinate;
-   }
-
    public double getDistance(final Location from) {
       return sqrt(pow(from.getX() - x, 2) + pow(from.getY() - y, 2));
-   }
-
-   public double getX() {
-      return this.x;
-   }
-
-   public double getY() {
-      return this.y;
    }
 }
