c4b89997b7c723984f44e611391bd37845c2bb6c
Chris <cvaranes@calpoly.edu>
2017-12-03 18:38:28 -0800

    checkstyle

--- app/src/main/java/cpe307/team6/toweroffense/game/views/GameSurface.java
+++ app/src/main/java/cpe307/team6/toweroffense/game/views/GameSurface.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
