6616f23c58edaf9add2e101d12fa41b24460eabd
Caleb Kim <me@calebkim.me>
2017-12-03 16:55:27 -0800

    Add BaseTest suite class

---
+++ app/src/test/java/cpe307/team6/toweroffense/game/AllBaseUnitTests.java
@@ -0,0 +1,13 @@
+package cpe307.team6.toweroffense.game;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BaseTakeDamageTest.class, BaseGetPathTest.class })
+
+// Ian Watts
+public class AllBaseUnitTests {
+
+}
---
+++ app/src/test/java/cpe307/team6/toweroffense/game/BaseGetPathTest.java
@@ -0,0 +1,15 @@
+package cpe307.team6.toweroffense.game;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class BaseGetPathTest {
+   @Test
+   public void getEmptyPath() {
+      Base base = new Base(300, new Location(0,0), Collections.<Location>emptyList());
+      assertEquals(Collections.<Location>emptyList(), base.getPath());
+   }
+}
