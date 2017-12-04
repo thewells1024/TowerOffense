9bcbe148b063e6017e17cbcb671a0989f36eecf6
aross885 <32276741+aross885@users.noreply.github.com>
2017-12-03 19:37:41 -0800

    fixed canHoldTower check #100

--- app/src/test/java/cpe307/team6/toweroffense/game/MapTest.java
+++ app/src/test/java/cpe307/team6/toweroffense/game/MapTest.java
@@ -23,7 +23,7 @@ public class MapTest {
     }
 
     @Test
-    public void testMapCreate_2() {
+    public void testMapCreate_6() {
         List<List<Map.Tile>> createdMap = Map.createMap(2,2,asList(new Location(0,0),
                 new Location(0,1)));
         assertEquals(2, createdMap.get(0).size());
@@ -33,27 +33,27 @@ public class MapTest {
     public void testMapCreate() {
         List<List<Map.Tile>> createdMap = Map.createMap(2,2,asList(new Location(0,0), 
                 new Location(0,1)));
-        assertEquals(createdMap.get(0).get(0).canHoldTower, false);
+        assertEquals(createdMap.get(0).get(0).canHoldTower(), false);
     }
     
     @Test 
     public void testMapCreate_3() {
         List<List<Map.Tile>> createdMap = Map.createMap(5,5,asList(new Location(0,0),
                 new Location(0,1)));
-        assertEquals(createdMap.get(1).get(0).canHoldTower, true);
+        assertEquals(createdMap.get(1).get(0).canHoldTower(), true);
     }
 
     @Test
     public void testMapCreate_4() {
         List<List<Map.Tile>> createdMap = Map.createMap(5,5,asList(new Location(0,0),
                 new Location(0,1)));
-        assertEquals(createdMap.get(1).get(1).canHoldTower, true);
+        assertEquals(createdMap.get(1).get(1).canHoldTower(), true);
     }
     
     @Test
     public void testMapCreate_5() {
         List<List<Map.Tile>> createdMap = Map.createMap(5,5,asList(new Location(0,0),
                 new Location(0,1)));
-        assertEquals(createdMap.get(0).get(1).canHoldTower, false);
+        assertEquals(createdMap.get(0).get(1).canHoldTower(), false);
     }
 }
