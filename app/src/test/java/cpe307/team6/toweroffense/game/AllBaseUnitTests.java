01d34464164b3dce1e3aa5c4a9cd8379ab4f5f2f
Chris Varanese <cvaranes@calpoly.edu>
2017-12-03 17:06:17 -0800

    Update AllBaseUnitTests.java

--- app/src/test/java/cpe307/team6/toweroffense/game/AllBaseUnitTests.java
+++ app/src/test/java/cpe307/team6/toweroffense/game/AllBaseUnitTests.java
@@ -7,6 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ BaseTakeDamageTest.class, BaseGetPathTest.class })
 
+/* Caleb Kim */
 public class AllBaseUnitTests {
 
 }
